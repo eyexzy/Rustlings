589a523fe58a4fc1f7518e7d8f524753dde3169f
Oleksii Molchanov <eyexzy@gmail.com>
2024-11-24 18:36:52 +0200

    Видалено всі файли

--- clippy/target/debug/deps/clippy1-883cc90203ad99a5.d
+++
@@ -1,9 +0,0 @@
-C:\Users\pc\kma_rustlang_course_2024_Oleksii_Molchanov\rustlings\exercises\clippy\target\debug\deps\libclippy1-883cc90203ad99a5.rmeta: clippy1.rs Cargo.toml
-
-C:\Users\pc\kma_rustlang_course_2024_Oleksii_Molchanov\rustlings\exercises\clippy\target\debug\deps\clippy1-883cc90203ad99a5.d: clippy1.rs Cargo.toml
-
-clippy1.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
